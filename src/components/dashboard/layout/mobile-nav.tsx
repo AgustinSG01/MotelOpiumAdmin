2ade9ce9c5336a4b8b24515e2f3f024ba7f5ff2b
AgustinSG01 <sousagodoyagustin@gmail.com>
2025-01-07 09:02:52 +0100

    fix

--- src/components/dashboard/layout/mobile-nav.tsx
+++ src/components/dashboard/layout/mobile-nav.tsx
@@ -5,12 +5,10 @@ import RouterLink from 'next/link';
 import { usePathname } from 'next/navigation';
 import { getGerenteInService } from '@/utils/get-gerente-in-service';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-import { ArrowSquareUpRight as ArrowSquareUpRightIcon } from '@phosphor-icons/react/dist/ssr/ArrowSquareUpRight';
 import { CaretUpDown as CaretUpDownIcon } from '@phosphor-icons/react/dist/ssr/CaretUpDown';
 
 import type { NavItemConfig } from '@/types/nav';
