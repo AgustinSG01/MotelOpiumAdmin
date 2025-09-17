f6a9bd50604546c317d050ee8f2d1cdf4196a200
Nicolás Rivero <109702352+riveroide@users.noreply.github.com>
2025-09-17 15:45:01 -0400

    Update CalcFinalRecepTable.tsx

--- src/components/dashboard/controles/CalcFinalRecepTable.tsx
+++ src/components/dashboard/controles/CalcFinalRecepTable.tsx
@@ -118,7 +118,7 @@ export function CalcFinalRecepTable({
         onRowsPerPageChange={handleChangeRowsPerPage}
         page={page}
         rowsPerPage={rowsPerPage}
-        rowsPerPageOptions={[5, 10, 25]}
+        rowsPerPageOptions={[5, 10, 25, 50, 100]}
       />
     </Card>
   );
